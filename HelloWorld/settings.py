1a920d6b5a12158842da04c906b32c2d8a13dee0
ZZ80159 <1784556173@qq.com>
2022-05-28 19:42:52 +0800

    2022-05-28 19:42

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-&tv87w7xhr$!yv5z2ff76@)^odh!xh77bm@g)(b)vkmd!lr4y*
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -64,12 +62,14 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            "libraries": {  # 添加这边三行配置
+                'my_tags': 'templatetags.my_tags'  # 添加这边三行配置
+            }  # 添加这边三行配置
         },
     },
 ]
 
 WSGI_APPLICATION = 'HelloWorld.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -80,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -100,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -111,7 +109,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 def hello(request):
     context = {}
-    context['hello'] = 'Hello World!'
+    context['hello'] = '走进Django世界！'
+    context['太祖'] = '为有牺牲多壮志，敢教日月让换新天'
+    context['views_str'] = "<a href='https://www.runoob.com/'>菜鸟首页</a>"
     return render(request, 'hello.html', context)
