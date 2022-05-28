200ecdddbe954a7a14bcdc630a21af80b5712c01
ZZ80159 <1784556173@qq.com>
2022-05-28 19:53:35 +0800

    新增静态资源配置

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -119,3 +119,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 静态资源文件路径配置
+STATIC_URL = '/static/'  # 别名
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "statics"),
+]
